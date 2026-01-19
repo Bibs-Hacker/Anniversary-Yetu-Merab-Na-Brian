93c748bf03e3c6179aee886a5c20129145fef76a
Bibs-Hacker <199905918+Bibs-Hacker@users.noreply.github.com>
2026-01-19 18:16:03 +0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = 'eternal-bond-cache-v1';
+const urlsToCache = [
+  '/',
+  'index.html',
+  'https://cdn.emailjs.com/dist/email.min.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js',
+  'https://files.catbox.moe/08b4pp.jpg'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
